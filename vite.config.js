70dbb14339f929f0e71cbbe859777f2f3954ba65
Manjunath <manjunathbraja@gmail.com>
2026-02-12 03:19:42 +0530

    Fix base path for production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+
 
 export default defineConfig({
-  base: '/image-crypto/',
+  base: process.env.NODE_ENV === 'production' ? '/image-crypto/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
